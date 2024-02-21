2b181b191cc736cb8a8ce5fd85d37fcc0dd71afa
Caio Martins Mendes Rodrigues <caio03martins@gmail.com>
2024-02-21 16:07:52 -0300

    :wrench: chore: Styled components setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
-function App() {
-  return <div></div>;
-}
+import styled from "styled-components";
 
+const StyledDiv = styled.div`
+  color: red;
+  background-color: #808080;
+  width: 100%;
+  display: flex;
+`;
+
+const App = () => {
+  return <StyledDiv>Hello, Styled Components!</StyledDiv>;
+};
 export default App;
---
+++ src/styles/reset.ts
@@ -0,0 +1,58 @@
+import { createGlobalStyle } from "styled-components";
+
+const ResetStyles = createGlobalStyle`
+
+/*
+
+@layer base {
+  * {
+    @apply box-border;
+  }
+  body {
+    @apply h-full overflow-x-hidden bg-zinc-950 text-zinc-50 flex-1 flex flex-col max-w-screen-2xl mx-auto w-full justify-center items-center;
+  }
+  html {
+    @apply h-full;
+  }
+} */
+
+html,
+body{
+    padding:0;
+    margin:0;
+    height:100%;
+    width:100%;
+    background-color:#09090b;
+    color:#fafafa;
+
+}
+
+
+
+
+#root{
+    height:100%;
+    overflow-x:hidden;
+    background-color:#09090b;
+    color:#fafafa;
+    display:flex;
+    flex-direction:column;
+    max-width:1536px;
+    margin:0 auto;
+    width:100%;
+    justify-content:center;
+    align-items:center;
+}
+a{
+    color:inherit;
+    text-decoration:none
+}
+
+*{
+    box-sizing:border-box;
+}
+
+
+`;
+
+export default ResetStyles;
