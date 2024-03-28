f2950c2f48c5a425f99f853441f61f54a8761b61
Caio Martins Mendes Rodrigues <caio03martins@gmail.com>
2024-03-27 22:11:16 -0300

    :wrench: chore: Add zinc colors to styled theme

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -5,5 +5,8 @@ export default {
     primary_light: "RGB(75, 134, 108)",
     dark: "#404040",
     background: "#2d3439",
+    zinc_600: "#52525b",
+    zinc_700: "#3f3f46",
+    zinc_800: "#27272a",
   },
 };
